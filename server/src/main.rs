c14ccf2d6d02801210eed1d23fe00bb275e33726
zhangyemengren <624593758@qq.com>
2023-12-01 17:14:40 +0800

    feat: 连接池

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,18 +1,34 @@
 use std::io::{BufRead, BufReader, Write};
 use std::net::{Shutdown, TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 use std::thread;
+use std::thread::{JoinHandle};
 use std::time::Duration;
+
+const MAX_CONNECT: usize = 2;
 
 fn main() {
     // 监听
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    // 使用arc + mutex 代替全局变量
+    let pool: Arc<Mutex<Vec<JoinHandle<()>>>> = Arc::new(Mutex::new(Vec::new()));
     // 接收并处理每一个请求
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(move || {
+        let pool_clone = pool.clone();
+        let mut pool = pool.lock().unwrap();
+        if pool.len() >= MAX_CONNECT {
+            println!("连接数过多");
+            continue;
+        }
+        let handle = thread::spawn(move || {
             println!("New connection: {}", stream.peer_addr().unwrap());
             handle_connection(stream);
+            let mut pool = pool_clone.lock().unwrap();
+            pool.retain(|x| x.thread().id() != thread::current().id());
         });
+
+        pool.push(handle);
     }
 }
 // 处理请求 读取并返回
